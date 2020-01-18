d3f8fa4eee2c64d1f1a73439dbbb41ba3a9e8513
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-18 19:09:57 +1000

    Added Users Posts

--- client/src/components/pages/SingleUser.jsx
+++ client/src/components/pages/SingleUser.jsx
@@ -7,6 +7,8 @@ import { observer } from 'mobx-react';
 import Navigation from '../layout/Navbar';
 import ProfileHeader from '../ProfileHeader';
 import SuggestedUsers from '../explore/SuggestedUsers';
+import PostList from '../explore/PostList';
+import Footer from '../layout/Footer';
 
 import { Container, Row, Col } from 'react-bootstrap';
 
@@ -18,6 +20,8 @@ let SingleUser = props => {
 
     const activeUser = appStore.users ? appStore.users.filter(user => user._id === props.match.params.id)[0] : null;
 
+    const postsCount = activeUser && appStore.posts ? appStore.posts.filter(post => post.postedBy.id === props.match.params.id).length : 0;
+
     return (
         <div>
             <Navigation />
@@ -25,13 +29,17 @@ let SingleUser = props => {
             <Container className="mt-5">
                 <Row>
                     <Col md={8}>
-                        {/* Users Post Component here*/}
+                        <h1 className="display-4 mb-4">
+                            {activeUser ? `${activeUser.firstname}'s Posts (${postsCount} posts)` : 'Loading'}
+                        </h1>
+                        <PostList userId={props.match.params.id} />
                     </Col>
                     <Col md={4}>
                         <SuggestedUsers />
                     </Col>
                 </Row>
             </Container>
+            <Footer type="large" />
         </div>
     )
 }
