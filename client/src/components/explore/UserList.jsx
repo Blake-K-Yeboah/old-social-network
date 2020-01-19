013052532917091e7c67be9037fc047ec52bd7cf
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-19 14:59:08 +1000

    Added Dark Theme

--- client/src/components/explore/UserList.jsx
+++ client/src/components/explore/UserList.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 
 import appStore from '../../store';
 
@@ -8,6 +8,8 @@ import { NavLink } from 'react-router-dom';
 
 import { ListGroup, Media, Spinner } from 'react-bootstrap';
 
+import classNames from 'classnames';
+
 let UserList = () => {
 
     useEffect(() => {
@@ -16,40 +18,45 @@ let UserList = () => {
 
     const users = appStore.users;
 
+    const condition = appStore.auth.user.preferredTheme === 'Dark';
+
     return (
         <React.Fragment>
-            <h1 className="display-4">Browse Users</h1>
-            
+            <h1 className={classNames("display-4", { 'text-light': condition })}>Browse Users</h1>
+
             <ListGroup>
-                    {users ? users.map(user => {
-                        return (
-                            <ListGroup.Item key={user._id} className="py-3" variant={user._id === appStore.auth.user.id ? 'secondary' : 'light'}>
-                                <Media>
+                {users ? users.map(user => {
 
-                                    <img src={`/uploads/profile/${user.profileIcon}`} alt={`${user.firstname} Profile Icon`} className="mr-3" style={{ width: '64px', borderRadius: '50%' }} />
+                    const activeUserCondition = user._id === appStore.auth.user.id;
 
-                                    <Media.Body>
+                    return (
+                        <ListGroup.Item key={user._id} className={classNames("py-3", { 'bg-dark': condition })} variant={activeUserCondition ? 'secondary' : 'light'}>
+                            <Media>
 
-                                        <h5 className="mt-0">
+                                <img src={`/uploads/profile/${user.profileIcon}`} alt={`${user.firstname} Profile Icon`} className="mr-3" style={{ width: '64px', borderRadius: '50%' }} />
 
-                                            <NavLink to={`/user/${user._id}`} className="text-dark">
+                                <Media.Body className={classNames({ 'text-muted': condition })}>
 
-                                                {`${user.firstname} ${user.lastname}`}
+                                    <h5 className="mt-0">
 
-                                            </NavLink>
+                                        <NavLink to={`/user/${user._id}`} className={classNames({ "text-dark": !condition, "text-light": condition })}>
 
-                                        </h5>
+                                            {`${user.firstname} ${user.lastname}`}
 
-                                        {user.bio ? user.bio : 'No Bio'}
+                                        </NavLink>
 
-                                    </Media.Body>
+                                    </h5>
 
-                                </Media>
-                            </ListGroup.Item>
-                        )
-                    }) : <Spinner animation="border" variant="danger" className="mx-auto my-2" />}
-                </ListGroup>
-            
+                                    {user.bio ? user.bio : 'No Bio'}
+
+                                </Media.Body>
+
+                            </Media>
+                        </ListGroup.Item>
+                    )
+                }) : <Spinner animation="border" variant="danger" className="mx-auto my-2" />}
+            </ListGroup>
+
         </React.Fragment>
     )
 }
