11d5f3775dc5fd34dc88c4f51f2f1bebd1576fce
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-19 19:51:43 +1000

    Added Not Found Header for not found page

---
+++ client/src/components/NotFoundHeader.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react'
+
+import appStore from '../store';
+
+import { observer } from 'mobx-react';
+
+// Import React-Bootstrap jumbotron
+import { Jumbotron } from 'react-bootstrap';
+
+import { NavLink } from 'react-router-dom';
+
+import classNames from 'classnames';
+
+const NotFoundHeader = () => {
+    // Define User
+    let user = appStore.auth.user;
+
+    // If The User has dark theme selected change body color
+    useEffect(() => {
+        if (user && JSON.parse(JSON.stringify(user)).preferredTheme === 'Dark') {
+            document.body.style.background = "#343A40";
+        } else {
+            document.body.style.background = "#E9ECEF";
+        }
+    }, [user]);
+
+    // Set Theme class (dark or light) based on user
+    const themeClass = appStore.auth.user && JSON.parse(JSON.stringify(appStore.auth.user)).preferredTheme === 'Dark' ? 'text-light' : '';
+
+    // Set Bg Class (dark or light) based on user
+    const bgClass = appStore.auth.user && JSON.parse(JSON.stringify(appStore.auth.user)).preferredTheme === 'Dark' ? 'bg-dark' : '';
+
+    return (
+        <Jumbotron className={bgClass}>
+            <h1 className={classNames("text-center", "display-4", "my-5", themeClass)}>404 - Page Not Found</h1>
+            <p className="lead text-center">
+                <NavLink to="/" className="text-danger">Redirect to Home</NavLink>
+            </p>
+        </Jumbotron>
+    )
+}
+
+export default observer(NotFoundHeader);
